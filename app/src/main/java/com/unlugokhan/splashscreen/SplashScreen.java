aa81b250c490cc6701bcd427c1d5dd214ae80c3b
gokhanunlu <52887587+gokhanunlu@users.noreply.github.com>
2024-01-14 03:21:12 +0300

    up

--- app/src/main/java/com/unlugokhan/splashscreen/SplashScreen.java
+++ app/src/main/java/com/unlugokhan/splashscreen/SplashScreen.java
@@ -1,21 +1,14 @@
 package com.unlugokhan.splashscreen;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.animation.ArgbEvaluator;
-import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.annotation.SuppressLint;
-import android.content.Context;
-import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.LinearInterpolator;
@@ -23,15 +16,7 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.DataSource;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.bumptech.glide.load.engine.GlideException;
-import com.bumptech.glide.request.RequestListener;
-import com.bumptech.glide.request.target.Target;
 import com.unlugokhan.splashscreen.model.DtoIntro;
 import com.unlugokhan.splashscreen.util.FixedSpeedScroller;
 import com.unlugokhan.splashscreen.util.Tools;
@@ -41,7 +26,6 @@ import com.unlugokhan.splashscreen.view.ViewPagerAdapter;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 @SuppressLint("CustomSplashScreen")
 public class SplashScreen extends AppCompatActivity {
@@ -55,7 +39,7 @@ public class SplashScreen extends AppCompatActivity {
     private int[] translationXValues = {-30, 40, 100, 160, 220};
     private int[] translationYValues = {-30, 40, 100, 160, 220};
     private int[] rotationValues = {245, 250, 255, 260, 265};
-    private int currentColor; // initial color is not set here
+    private int currentColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -113,8 +97,7 @@ public class SplashScreen extends AppCompatActivity {
             bottomProgressDots(position);
             animateImageViews(position);
             if (position == max_step - 1) {
-                btnNext.setText("BAŞLAYALIM !");
-
+                btnNext.setText(R.string.END_TEXT);
             } else {
                 btnNext.setText(getString(R.string.NEXT));
             }
@@ -138,6 +121,10 @@ public class SplashScreen extends AppCompatActivity {
             R.color.p4,
             R.color.p5
     };
+
+
+
+
 
     private void animateColorChange(ImageView imageView, int colorTo) {
         int colorFrom = currentColor;
